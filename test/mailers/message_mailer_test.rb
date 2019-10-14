e6509942cd905c6234dca5ce1afb3e78d542bbf4
Max Kelly <max@revelthat.com>
2019-10-14 21:32:43 +1100

    tried to fix contact database

--- test/mailers/message_mailer_test.rb
+++ test/mailers/message_mailer_test.rb
@@ -2,11 +2,21 @@ require 'test_helper'
 
 class MessageMailerTest < ActionMailer::TestCase
   test "contact_me" do
-    mail = MessageMailer.contact_me
-    assert_equal "Contact me", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    message = Message.new name:'Max',
+                          email: 'max@example.org',
+                          message: 'Hello There!'
+    
+    email = MessageMailer.contact_me(message)
+
+    assert_emails 1 do
+      email.deliver_now
+    end
+
+    assert_equal 'Message from www.murdo.ch', email.subject
+    assert_equal ['maxckel@gmail.com'], email.to
+    assert_equal ['max@example.org'], email.from
+    assert_match /hello, how are you doing?/, email.body.encoded
+           
   end
 
 end
